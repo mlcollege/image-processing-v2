12f50fe3fff8a7b71f2c6ebc164440af087f8662
Adam Kolar <adakolar@gmail.com>
2019-04-17 00:56:43 +0200

    feature: Setup workshop package

---
+++ setup.py
@@ -0,0 +1,43 @@
+import sys
+from setuptools.command.test import test as TestCommand
+from setuptools import setup, find_packages
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to pytest")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = ''
+
+    def run_tests(self):
+        import shlex
+        import pytest
+        errno = pytest.main(shlex.split(self.pytest_args))
+        sys.exit(errno)
+
+
+setup(
+    name='image_processing_workshop',
+    version='1.0',
+    description='Workshop custom tools',
+    author='Adam Kolar',
+    author_email='',
+    url='',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    scripts=[],
+    setup_requires=['pytest-runner'],
+    install_requires=[
+
+    ],
+    tests_require=['pytest'],
+    cmdclass={'test': PyTest},
+    include_package_data=True,
+    package_data={
+        # Add all files under data/ directory.
+        # This data will be part of this package.
+        # Access them with pkg_resources module.
+        # Folder with data HAVE TO be in some module, so dont add it to folder with tests, which SHOULD NOT be a module.
+    },
+)
